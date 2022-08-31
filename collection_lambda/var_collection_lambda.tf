0f1f079904e9beacfa2cb332d272f06b545ca585
lightlyticsDevops <devops@lightlytics.com>
2022-08-31 08:38:03 +0000

    Update version - build_number: 366

--- collection_lambda/var_collection_lambda.tf
+++ collection_lambda/var_collection_lambda.tf
@@ -2,7 +2,7 @@ variable "collection-lambda" {
   type = map(string)
   default = {
     "stage-collection_lambda_layer" = "ca0ea8926b716122f6824e453a1dfd4c"
-    "stage-collection_lambda_cloudwatch" = "c572fc2acd8ad1409e71991efcc5792f"
+    "stage-collection_lambda_cloudwatch" = "cab6efc34459a63d4b59214aaa2cdf8d"
     "prod-collection_lambda_layer" = "b1c737d066d8ba72d3cb71d0dcbb1e5a"
     "prod-collection_lambda_cloudwatch" = "e2d4381c21fa022bbbb510224dba3642"
   }
--- flowlogs_lambda/var_flowlogs_lambda.tf
+++ flowlogs_lambda/var_flowlogs_lambda.tf
@@ -2,7 +2,7 @@ variable "flowlogs-lambda" {
   type = map(string)
   default = {
     "stage-collection_lambda_layer" = "ca0ea8926b716122f6824e453a1dfd4c"
-    "stage-collection_lambda_flowlogs" = "c572fc2acd8ad1409e71991efcc5792f"
+    "stage-collection_lambda_flowlogs" = "cab6efc34459a63d4b59214aaa2cdf8d"
     "prod-collection_lambda_layer" = "b1c737d066d8ba72d3cb71d0dcbb1e5a"
     "prod-collection_lambda_flowlogs" = "e2d4381c21fa022bbbb510224dba3642"
   }
