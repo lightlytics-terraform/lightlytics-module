5c61eda00af26bec6406fc5f0b172881782e3e28
lightlyticsDevops <devops@lightlytics.com>
2022-10-16 08:21:58 +0000

    Update version - build_number: 80

--- flowlogs_lambda/var_flowlogs_lambda.tf
+++ flowlogs_lambda/var_flowlogs_lambda.tf
@@ -3,7 +3,7 @@ variable "flowlogs-lambda" {
   default = {
     "stage-collection_lambda_layer" = "3dadb74231727ce3a5ab33b2aa28ad24"
     "stage-collection_lambda_flowlogs" = "ad43ff0c9e398a607aea96e0b46c8022"
-    "prod-collection_lambda_layer" = "d08e9f807334125d8d44cc1db7fcd155"
-    "prod-collection_lambda_flowlogs" = "cab6efc34459a63d4b59214aaa2cdf8d"
+    "prod-collection_lambda_layer" = "7abfe7394a5b2e3b0194ee7c39f69e6c"
+    "prod-collection_lambda_flowlogs" = "ad43ff0c9e398a607aea96e0b46c8022"
   }
 }
