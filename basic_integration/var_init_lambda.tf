bff65a7965caa1484440ae5a167ab4e96b4665ae
lightlyticsDevops <devops@lightlytics.com>
2023-05-02 14:58:31 +0000

    Update version - build_number: 448

--- basic_integration/var_init_lambda.tf
+++ basic_integration/var_init_lambda.tf
@@ -1,7 +1,7 @@
 variable "init-lambda" {
   type = map(string)
   default = {
-    "stage-init_lambda" = "189efd9da5eb5fc963ab1c4a0bd652e5"
+    "stage-init_lambda" = "4acebcd2bf4ed2d30dbf2bbf3e33434c"
     "prod-init_lambda" = "189efd9da5eb5fc963ab1c4a0bd652e5"
   }
 }
