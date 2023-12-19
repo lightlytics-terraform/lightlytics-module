b7c64844be2ea9d59e9fc7e19e76ac7306a7fc5d
lightlyticsDevops <devops@lightlytics.com>
2023-12-19 10:57:13 +0000

    Update version - build_number: 485

--- collection_lambda/var_collection_lambda.tf
+++ collection_lambda/var_collection_lambda.tf
@@ -1,7 +1,7 @@
 variable "collection-lambda" {
   type = map(string)
   default = {
-    "stage-collection_lambda_layer" = "3c6bab0d82b651e43ef7811b67ce6ff2"
+    "stage-collection_lambda_layer" = "7defdd088df940d698d7a736fc3eb8d2"
     "stage-collection_lambda_cloudwatch" = "d3422a591b61069f6994aa3ee1e6e12d"
     "prod-collection_lambda_layer" = "c6089f48aeb2a3bf17a77f936ea0b344"
     "prod-collection_lambda_cloudwatch" = "250b54a2499d95450569d037be0d17eb"
