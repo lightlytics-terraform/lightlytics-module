4f0977001b1255246da1c75a4401ac05bda00741
lightlyticsDevops <devops@lightlytics.com>
2024-01-18 14:58:00 +0000

    Update version - build_number: 145

--- iam_activity_lambda/var_iam_activity_lambda.tf
+++ iam_activity_lambda/var_iam_activity_lambda.tf
@@ -3,7 +3,7 @@ variable "iam-activity-lambda" {
   default = {
     "stage-collection_lambda_layer" = "7defdd088df940d698d7a736fc3eb8d2"
     "stage-collection_lambda_iam_activity" = "d3422a591b61069f6994aa3ee1e6e12d"
-    "prod-collection_lambda_layer" = "0cbc3720df48462db9fbe4b8d60b934e"
+    "prod-collection_lambda_layer" = "bc7e4ff0ef7103ca2d656a0d8f40096a"
     "prod-collection_lambda_iam_activity" = "d3422a591b61069f6994aa3ee1e6e12d"
   }
 }
