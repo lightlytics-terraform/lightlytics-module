2e9f86f5777200a219b2158c97c692bf4fb8a575
lightlyticsDevops <devops@lightlytics.com>
2024-10-13 08:51:26 +0000

    Update version - build_number: 2

--- basic_integration/var_init_lambda.tf
+++ basic_integration/var_init_lambda.tf
@@ -1,7 +1,7 @@
 variable "init-lambda" {
   type = map(string)
   default = {
-    "stage-init_lambda" = "4acebcd2bf4ed2d30dbf2bbf3e33434c"
+    "stage-init_lambda" = "07b5b792015ce262d599b842372c5af5"
     "prod-init_lambda" = "4acebcd2bf4ed2d30dbf2bbf3e33434c"
   }
 }
--- flowlogs_lambda/var_flowlogs_lambda.tf
+++ flowlogs_lambda/var_flowlogs_lambda.tf
@@ -1,8 +1,8 @@
 variable "flowlogs-lambda" {
   type = map(string)
   default = {
-    "stage-collection_lambda_layer" = "45045478d41c1e6a160e3fe80597c3d2"
-    "stage-collection_lambda_flowlogs" = "7cb5ab0ae7fce48eb44277ae6ed572e2"
+    "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
+    "stage-collection_lambda_flowlogs" = "50d1189c896f480d8d71b4c9f06f6da2"
     "prod-collection_lambda_layer" = "24ad10212c195b9a805df9b82493877b"
     "prod-collection_lambda_flowlogs" = "6c0c66c12749eb83543b83c0b2c27e69"
   }
