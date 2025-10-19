ac236c4da6328c95eecc0bd7bb5dc31196cff9cc
Streamsec <devops@lightlytics.com>
2025-10-19 08:16:47 +0000

    Update terraform - build_number: 219

--- collection_lambda/var_collection_lambda.tf
+++ collection_lambda/var_collection_lambda.tf
@@ -3,7 +3,7 @@ variable "collection-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_cloudwatch" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "903c7914ee2b25bcadaf7b814fa22028"
+    "prod-collection_lambda_layer" = "2d844926048287f7e3893903c140a1a4"
     "prod-collection_lambda_cloudwatch" = "42f36ccb52027140918f0b5a15700c3d"
   }
 }
