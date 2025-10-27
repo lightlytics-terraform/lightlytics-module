1fdc83d4b20d40469728a829d743155151b43128
Streamsec <devops@lightlytics.com>
2025-10-27 20:30:53 +0000

    Update terraform - build_number: 221

--- iam_activity_lambda/var_iam_activity_lambda.tf
+++ iam_activity_lambda/var_iam_activity_lambda.tf
@@ -3,7 +3,7 @@ variable "iam-activity-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_iam_activity" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "2d844926048287f7e3893903c140a1a4"
+    "prod-collection_lambda_layer" = "4de4712aad794f2ae856b10c4609f7c1"
     "prod-collection_lambda_iam_activity" = "42f36ccb52027140918f0b5a15700c3d"
   }
 }
