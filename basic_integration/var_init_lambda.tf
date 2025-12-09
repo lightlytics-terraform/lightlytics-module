f89219192cc54b244b324ba73c4dabef8e39de1a
Streamsec <devops@lightlytics.com>
2025-12-09 12:24:18 +0000

    Update terraform - build_number: 223

--- basic_integration/var_init_lambda.tf
+++ basic_integration/var_init_lambda.tf
@@ -2,6 +2,6 @@ variable "init-lambda" {
   type = map(string)
   default = {
     "stage-init_lambda" = "07b5b792015ce262d599b842372c5af5"
-    "prod-init_lambda" = "07b5b792015ce262d599b842372c5af5"
+    "prod-init_lambda" = "bfdc386aa8c3f30a77eb47336f77c803"
   }
 }
