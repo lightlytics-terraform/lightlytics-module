a4a0bb5f169bb7590a993e92e20755f8044461e3
Platform Jenkins <devops@lightlytics.com>
2026-02-18 15:08:19 +0000

    Update terraform - build_number: 232

--- iam_activity_lambda/var_iam_activity_lambda.tf
+++ iam_activity_lambda/var_iam_activity_lambda.tf
@@ -3,7 +3,7 @@ variable "iam-activity-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_iam_activity" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "920290e933bd47a17b0634bcd140287e"
+    "prod-collection_lambda_layer" = "6de35e40e3665eac2141879b71d3e7eb"
     "prod-collection_lambda_iam_activity" = "ac64c01a935bb98a8d9f1bdbe321b715"
   }
 }
