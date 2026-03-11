65783a91c79b2949a53973b04382d3dc95400d00
lightlyticsDevops <77959471+lightlyticsDevops@users.noreply.github.com>
2026-03-11 12:20:19 +0200

    Update terraform - build_number: 236 (#19)

--- iam_activity_lambda/var_iam_activity_lambda.tf
+++ iam_activity_lambda/var_iam_activity_lambda.tf
@@ -3,7 +3,7 @@ variable "iam-activity-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_iam_activity" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "37d464e942b681648420fcf9c123955d"
+    "prod-collection_lambda_layer" = "99b36f02a5e21f5c87673ffc739e3f8f"
     "prod-collection_lambda_iam_activity" = "5e386f9a6adfc2f7e71c45d03795366d"
   }
 }
