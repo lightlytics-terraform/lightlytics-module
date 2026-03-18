67e72afb7722d9efbd9147988e1fe3823e8746a8
Platform Jenkins <devops@lightlytics.com>
2026-03-18 15:24:55 +0000

    Update terraform - build_number: 241

--- collection_lambda/var_collection_lambda.tf
+++ collection_lambda/var_collection_lambda.tf
@@ -3,7 +3,7 @@ variable "collection-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_cloudwatch" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "3b9c180d67b54886ef0fb8af40843e10"
-    "prod-collection_lambda_cloudwatch" = "5e386f9a6adfc2f7e71c45d03795366d"
+    "prod-collection_lambda_layer" = "6dbca45610bf4933b39a954f46c056db"
+    "prod-collection_lambda_cloudwatch" = "8c421c07f2bf89f05c00221fc34efd0c"
   }
 }
