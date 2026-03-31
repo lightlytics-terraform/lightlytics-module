faf409a5a873708cf1617e4a75218bdf0a7d8017
lightlyticsDevops <77959471+lightlyticsDevops@users.noreply.github.com>
2026-03-31 17:42:28 +0300

    Update terraform - build_number: 245 (#25)

--- flowlogs_lambda/var_flowlogs_lambda.tf
+++ flowlogs_lambda/var_flowlogs_lambda.tf
@@ -3,7 +3,7 @@ variable "flowlogs-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_flowlogs" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "9a230873bca52fb4367df7cb68c90b80"
+    "prod-collection_lambda_layer" = "0e45e270852117569cc20a8950544952"
     "prod-collection_lambda_flowlogs" = "509a92641d90cdd4d64c696db58aec06"
   }
 }
