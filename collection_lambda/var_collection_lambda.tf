7843486bcae6d12c0649f45189f2422e32c44f12
lightlyticsDevops <77959471+lightlyticsDevops@users.noreply.github.com>
2026-06-15 13:54:04 +0300

    Update terraform - build_number: 257 (#32)

--- collection_lambda/var_collection_lambda.tf
+++ collection_lambda/var_collection_lambda.tf
@@ -3,7 +3,7 @@ variable "collection-lambda" {
   default = {
     "stage-collection_lambda_layer" = "e1ac4ae88f17da52c54d0e3d90607a5b"
     "stage-collection_lambda_cloudwatch" = "50d1189c896f480d8d71b4c9f06f6da2"
-    "prod-collection_lambda_layer" = "0eee49d3582e038590f4d4096d72c6f9"
+    "prod-collection_lambda_layer" = "1e8d00c5c5513f60c336658713ee2cd5"
     "prod-collection_lambda_cloudwatch" = "74fe114d709c2efece809dfbd6a7577e"
   }
 }
